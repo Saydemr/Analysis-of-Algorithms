1e3a53fe880dd7c15f33b06edb9e02cb62c3c4e3
Abdullah Saydemir <abdullah.saydemir@ozu.edu.tr>
2021-05-30 15:46:19 -0700

    Necessary changes to make cpp code c++11 compatible

--- HW6/tsp.cpp
+++ HW6/tsp.cpp
@@ -5,6 +5,7 @@
 #include <fstream>
 #include <cmath>
 #include <climits>
+
 using namespace std;
 
 
@@ -12,13 +13,15 @@ struct Graph
 {
 	vector<tuple<int,int,int>> nodes;
 	vector<vector<int>> adj_matrix;
+	vector<int> visited_nodes;
 
 	int num_cities = 0;
+	int distance = 0;
 
 	Graph(int num_cities) : num_cities(num_cities) 
 	{
 		adj_matrix  = vector< vector<int> >(num_cities,vector< int >(num_cities,0));         // adjacency matrix, all values are initially 0
-		nodes       = vector< tuple< int,int > >();                                      // points in the graph
+		nodes       = vector< tuple< int,int,int > >();                                      // points in the graph
 	}
 
 	// this function calculates the distances between each
@@ -46,9 +49,58 @@ struct Graph
 		}
 	}
 
+
+	auto tsp(int start)                                      -> void
+	{
+		visited_nodes.push_back(start);
+
+		auto min_dist = 0;
+		auto index = 1;
+
+
+		for (auto i = 0; i < num_cities-1; ++i) 
+		{
+			min_dist = INT_MAX;
+
+			for (auto j = 0; j < num_cities; ++j) 
+			{	
+				if ( !is_visited(j) && adj_matrix[start][j] < min_dist) 
+				{
+					min_dist = adj_matrix[start][j];
+					index = j;
+				}
+			
+			}
+
+			distance += min_dist;
+			visited_nodes.push_back(index);
+			start = index;
+
+		}
+
+		distance += adj_matrix[visited_nodes.back()][visited_nodes[0]];
+	}
+
+
+	auto print_results(string& filename)                                   -> void
+	{
+		ofstream out;
+
+		out.open(filename, ios::trunc);
+
+		out << distance << endl;
+
+		for (int item : visited_nodes) 
+		{
+			out << item << endl;
+		}
+
+		out.close();
+	}
+
 	// utility function to print adjacency matrix
 	// not required in HW but useful for debugging
-	void print() const
+	void print_matrix() const
 	{
 		for (int i = 0; i < num_cities; ++i)
 		{
@@ -62,22 +114,31 @@ struct Graph
 
 
 private:
+
 	// calculate the distance between two points and
 	// round it to the nearest integer
-	int nearest_int(int x1, int y1, int x2, int y2)
+	auto nearest_int(int x1, int y1, int x2, int y2)                                   -> int
 	{
 		return round(sqrt((x1 - x2)*(x1 - x2) + (y1-y2) * (y1-y2)));
 	}
+
+	auto is_visited(int i)                                                                -> bool
+	{
+		for (int item : visited_nodes) 
+		{
+			if (item == i) return true;
+		}
+		return false;
+	}
 };
-
-
 
 
 
 int main(int argc, char const *argv[])
 {
 	
-	string testfile = argv[1];                       // Test file is always "graph.txt". No need for argv
+	string testfile = argv[1];
+
     ifstream file(testfile.c_str());
 
     if (file.is_open())                                    // Check if the file is correctly opened.
@@ -88,6 +149,10 @@ int main(int argc, char const *argv[])
         cout << "Error: cannot read the test file!" << endl;
         return -1;
     }
+
+    string outputfile = argv[1];
+	outputfile.append(".tour");
+	
 
     auto num_cities = 0;
     cin >> num_cities;
@@ -105,9 +170,13 @@ int main(int argc, char const *argv[])
     	graph.nodes.push_back(make_tuple(id,x,y));
     }
 
+    graph.calculate_weights();
+   // graph.print_matrix();
 
-    graph.print();
+    auto start = rand() % num_cities;
 
+    graph.tsp(start);
+    graph.print_results(outputfile);
 
 	return 0;
 }
